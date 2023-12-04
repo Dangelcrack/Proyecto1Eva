fadaa9393a97e45730dbfb5f0b7350efe50bf5b0
Dangelcrack <79106503+Dangelcrack@users.noreply.github.com>
2023-12-04 11:26:03 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Model/Main.java

--- Model/Main.java
+++ Model/Main.java
@@ -4,11 +4,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int n1 = 0;
-        int n2 = 0;
+        int n1;
+        int n2;
         Scanner scanner = new Scanner(System.in);
         n2 = scanner.nextInt();
         n1 = scanner.nextInt();
+        /*
+        texto
+         */
         System.out.println(n1+n2);
     }
 }

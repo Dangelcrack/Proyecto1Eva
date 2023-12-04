a905454c6c7982c8a588f93f676726b0080f5b2a
Dangelcrack <angelguerrero540@gmail.com>
2023-12-04 11:19:38 +0100

    main

---
+++ Model/Main.java
@@ -0,0 +1,14 @@
+package Model;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n1 = 0;
+        int n2 = 0;
+        Scanner scanner = new Scanner(System.in);
+        n2 = scanner.nextInt();
+        n1 = scanner.nextInt();
+        System.out.println(n1+n2);
+    }
+}

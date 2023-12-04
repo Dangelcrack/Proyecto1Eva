9f47ddfc915f148d4a8a8719cf0c4672018458e5
Dangelcrack <angelguerrero540@gmail.com>
2023-12-04 10:57:23 +0100

    carta

--- Model/Card.java
+++ Model/Card.java
@@ -1,2 +1,49 @@
-package Model;public class Card {
+package Model;
+
+import java.util.Objects;
+
+public class Card {
+    private int value;
+    private String suit;
+
+    Card() {
+        this(0, "");
+    }
+
+    public Card(int value, String suit) {
+        this.value = value;
+        this.suit = suit;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Card card = (Card) o;
+        return value == card.value && Objects.equals(suit, card.suit);
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "value=" + value +
+                ", suit='" + suit + '\'' +
+                '}';
+    }
 }
